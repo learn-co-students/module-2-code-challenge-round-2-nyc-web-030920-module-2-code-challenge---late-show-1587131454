cd4ffd325dd68e9ddb93753eaa313eb60680d530
stephenverdi95 <stephenverdi152@gmail.com>
2020-04-17 11:53:38 -0400

    all deliverables working

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,6 +1,7 @@
 class Appearance < ApplicationRecord
     validates :rating, numericality: {greater_than_or_equal_to: 1}
     validates :rating, numericality: {less_than_or_equal_to: 5}
+    validates :episode_id, uniqueness: {scope: :guest_id, message: "A guest can only appear on an episode once"}
     belongs_to :episode
     belongs_to :guest
 end
