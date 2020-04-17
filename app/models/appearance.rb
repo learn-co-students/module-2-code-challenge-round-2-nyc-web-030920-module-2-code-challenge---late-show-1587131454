a94bd994813c4376edaf245c45a71e8acfcf6f1a
Eliot Henson <eliotrhenson@gmail.com>
2020-04-17 11:19:38 -0400

    Validation working

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -2,7 +2,7 @@ class Appearance < ApplicationRecord
     belongs_to :guest
     belongs_to :episode
 
-    validates :numeric_rating, inclusion: { in: [-1, 5]}
+    validates :numeric_rating, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
 
 end
 
